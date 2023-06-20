61e57369d7ed6927ae0e89c3a30d25909f283a42
MarcoAurelioGR <marco-aurelio-paraiso@hotmail.com>
2023-06-20 19:06:09 -0300

    Mudanças no Navbar (hover e color), Toggler (shadow e color) e adicionando links do GitHub e Linkedin no Footer.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
 function OverMouse(elemento, rota){
     elemento.src = rota;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarToggler = document.querySelector(".navbar-toggler");
+    var navbarMenu = document.querySelector(".navbar-collapse");
+  
+    navbarMenu.addEventListener("click", function() {
+      navbarToggler.click(); 
+    });
+  });
